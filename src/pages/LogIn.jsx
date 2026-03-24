2c1cd3046c2133c530b5508255bda8c659427c2b
Nour <nour.alghadban@lau.edu>
2026-03-24 21:19:40 +0200

    Edited colors in forms input

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -47,36 +47,31 @@ const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   return (
 <section className="min-h-screen bg-[#f4f4f1] dark:bg-[#010513] flex items-center justify-center px-4 py-8 transition-colors duration-300">
       <div className="w-full max-w-[760px] bg-white dark:bg-[#10151C] rounded-[20px] shadow-[0_12px_40px_rgba(0,0,0,0.08)] px-6 py-10 md:px-12 md:py-12 flex flex-col items-center transition-colors duration-300">
-        {/* Logo */}
+
         <div className="w-24 h-24 rounded-full bg-[#43A047] flex items-center justify-center text-[2.6rem] mb-6">
           🌱
         </div>
 
-        {/* Title */}
         <h1 className="text-center text-[2rem] md:text-[2.8rem] font-extrabold text-[#23311f] dark:text-white">
           Welcome Back to Bloomly
         </h1>
 
-        {/* Subtitle */}
         <p className="mt-4 text-center text-[1rem] md:text-[1.1rem] text-[#6d7187] dark:text-[#b9c2b0]">
           Login to continue growing your garden
         </p>
 
-        {/* Floating icons */}
         <div className="flex gap-6 md:gap-8 my-10 md:my-14 text-[2.5rem] md:text-[3rem]">
           <span className="plant-icon">🌱</span>
           <span className="plant-icon">🌸</span>
           <span className="plant-icon">🌻</span>
         </div>
 
-        {/* Form */}
         <form
           className="w-full max-w-[580px] flex flex-col"
           onSubmit={handleSubmit}
           noValidate
         >
 
-          {/* Email */}
           <label className="text-sm font-bold text-[#23311f] dark:text-white mt-2">
             Email
           </label>
@@ -94,7 +89,6 @@ const [showConfirmPassword, setShowConfirmPassword] = useState(false);
             <p className="mt-10 text-gray-500 dark:text-[#b9c2b0] text-center"/>
           )}
 
-          {/* Password */}
           <label className="text-sm font-bold text-[#23311f] dark:text-white mt-2">
             Password
           </label>
@@ -122,7 +116,6 @@ const [showConfirmPassword, setShowConfirmPassword] = useState(false);
          </Link>
         </p>
 
-          {/* Button */}
           <button
             type="submit"
             className="mt-10 bg-[#43A047] text-white py-4 rounded-xl font-bold hover:opacity-90 transition"
@@ -131,7 +124,6 @@ const [showConfirmPassword, setShowConfirmPassword] = useState(false);
           </button>
         </form>
 
-        {/* Footer */}
         <p className="mt-10 text-gray-500 text-center">
           Don&apos;t have an account?{" "}
           <Link
