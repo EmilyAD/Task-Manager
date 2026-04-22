f915a28038794b4042f2ee6772c9d03b103db26f
May Ezzeddine <14silo.betters@icloud.com>
2026-04-22 15:58:00 +0300

    add api services and fix gitignore for env files

---
+++ src/api/authService.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+import API_BASE_URL from "./config";
+
+export const registerUser = async (email, password) => {
+  const res = await axios.post(`${API_BASE_URL}/register`, { email, password });
+  return res.data;
+};
+
+export const loginUser = async (email, password) => {
+  const res = await axios.post(`${API_BASE_URL}/login`, { email, password });
+  localStorage.setItem("token", res.data.token);
+  return res.data;
+};
+
+export const logoutUser = () => {
+  localStorage.removeItem("token");
+};
